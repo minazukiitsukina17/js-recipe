7987d667410b904ea625b5e1ef486886e3e2489d
minazukiitsukina17 <fernweh.forelsket@gmail.com>
2021-07-04 20:57:02 +0900

    ペアワーク完了

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -29,4 +29,20 @@ genkiFunction(100)
 $ cd webex/js-recipe
 $ git add .
 $ git commit -m "[3の倍数]を学習しました。"
+
 $ git push origin master
+
+const FizzBuzz = function (number) {
+    for (let i = 1; i <= number; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
+    } FizzBuzz(100)
+  };
+  
